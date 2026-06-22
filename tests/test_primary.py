eaf6a44c29974a86d8b6ee219a36fd6381b9a4e6
Utsavrawat02 <utsavrawat2003@gmail.com>
2026-06-22 14:56:05 +0530

    new test cases

--- tests/test_primary.py
+++ tests/test_primary.py
@@ -7,6 +7,7 @@ from app import main
 def test_multiply_zero():
     assert multiply(10, 0) == 0
 
+
 def test_health(capsys):
     with patch(
         "sys.argv",
@@ -19,6 +20,7 @@ def test_health(capsys):
     assert exit_code == 0
     assert "ok" in captured.out
 
+
 def test_invalid_args(capsys):
     with patch(
         "sys.argv",
@@ -29,6 +31,7 @@ def test_invalid_args(capsys):
 
     assert exit_code == 1
     assert "Arguments must be numeric" in captured.out
+
 
 def test_invalid_function(capsys):
     with patch(
@@ -42,6 +45,20 @@ def test_invalid_function(capsys):
     assert exit_code == 1
     assert "not found" in captured.out
 
+    
+def test_invalid_arg_count(capsys):
+    with patch(
+        "sys.argv",
+        ["app.py", "add", "2"]
+    ):
+        exit_code = main()
+
+    captured = capsys.readouterr()
+
+    assert exit_code == 1
+    assert "Usage: python main.py <function> <num1> <num2>" in captured.out
+
+
 def test_divide_by_zero(capsys):
     with patch("sys.argv", ["app.py", "div", "10", "0"]):
         exit_code = main()
@@ -49,6 +66,7 @@ def test_divide_by_zero(capsys):
 
     assert exit_code == 1
     assert "Cannot divide by zero" in captured.out
+
 
 def test_invalid_health_args(capsys):
     with patch(
@@ -58,6 +76,5 @@ def test_invalid_health_args(capsys):
         exit_code = main()
 
     captured = capsys.readouterr()
-
     assert exit_code == 1
-    assert "Usage: python main.py <function>" in captured.out
+    assert "Usage: python main.py <function> <num1> <num2>" in captured.out
